c30276d5ff9a34b765c73f7b5475c6dea3ff8d01
kindtiger <seonbeom2@gmail.com>
2023-11-24 09:07:39 +0900

    [add] Basic 학습
    - codeStructure 학습

--- client/main.js
+++ client/main.js
@@ -1,5 +1,19 @@
-const a = "hello";
 
-console.log(navigator.language);
+
+
+console.log('13');
+
+
+// alert('마우스 우클릭은 사용할 수 없습니다.')
+
+// console.log( prompt('당신의 이름은?')  );
+console.log( window.confirm('정말...우리 추억...지울꺼야..?')  );
+
+
+
+
+
+
+
 
 
