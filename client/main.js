f21b6bea61b01990d7a5dbe89260d006947d39f5
kindtiger <seonbeom2@gmail.com>
2023-12-05 15:52:28 +0900

    [learn] basic 학습
    - 재귀 함수
    - 객체란?
       - 제거와 삭제
       - 배열의 구조 분해 할당
       - 객체의 구조 분해 할당

--- client/main.js
+++ client/main.js
@@ -7,7 +7,7 @@ console.log('13');
 // alert('마우스 우클릭은 사용할 수 없습니다.')
 
 // console.log( prompt('당신의 이름은?')  );
-console.log( window.confirm('정말...우리 추억...지울꺼야..?')  );
+// console.log( window.confirm('정말...우리 추억...지울꺼야..?')  );
 
 
 
