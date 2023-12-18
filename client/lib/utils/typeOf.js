d4c6e63557295600d77e6825e9ff6271dc906e15
kindtiger <seonbeom2@gmail.com>
2023-12-18 15:16:31 +0900

    [learn] module
    - typeof 모듈 분리

--- client/lib/utils/typeOf.js
+++ client/lib/utils/typeOf.js
@@ -1,4 +1,17 @@
-export function isObject(data){
-  return Object.prototype.toString.call(data).slice(8,-1).toLowerCase() === 'object'
+
+
+function typeOf (data){
+  return Object.prototype.toString.call(data).slice(8,-1).toLowerCase()
 }
+
+
+
+export const isObject = data => typeOf(data) === 'object';
+export const isArray = data => typeOf(data) === 'array';
+export const isBoolean = data => typeOf(data) === 'boolean';
+export const isNull = data => typeOf(data) === 'null';
+export const isUndefined = data => typeOf(data) === 'undefined';
+export const isBinInt = data => typeOf(data) === 'bigint';
+export const isFunction = data => typeOf(data) === 'function';
+export const isSymbol = data => typeOf(data) === 'symbol';
 
