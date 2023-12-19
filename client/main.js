bc7d3e022c76b0757a00173e95c4c6e8f1b014e6
kindtiger <seonbeom2@gmail.com>
2023-12-19 14:50:33 +0900

    [learn] randomDice
    - memo

--- client/main.js
+++ client/main.js
@@ -1,86 +1,85 @@
+import {
+  memo,
+  getNode,
+  getNodes,
+  endScroll,
+  insertLast,
+  diceAnimation,
+  clearContents,
+} from "./lib/index.js";
 
+const [rollingButton, recordButton, resetButton] = getNodes(
+  ".buttonGroup > button"
+);
 
-
-import { diceAnimation, getNode, getNodes, insertLast } from "./lib/index.js";
-
-
-
-
-const [rollingButton,recordButton,resetButton] = getNodes('.buttonGroup > button');
-
-const recordListWrapper = getNode('.recordListWrapper');
+const recordListWrapper = getNode(".recordListWrapper");
 
 // [phase-1]
 // 1. 주사위 굴리기
 // 2. 주사위 굴리기 버튼을 클릭하면 diceAnimation() 실행될 수 있도록
 
-// toggle 
+// toggle
 // 1. 주사위 눈 값 가져오기
 // 2. 태그 만들기
-// 3. 뿌려주기 
+// 3. 뿌려주기
 
 let count = 0;
 let total = 0;
 
-
-function createItem(value){
+function createItem(value) {
   const template = `
     <tr>
       <td>${++count}</td>
       <td>${value}</td>
-      <td>${total += value}</td>
+      <td>${(total += value)}</td>
     </tr>
-  `
-  return template
+  `;
+  return template;
 }
 
-function renderRecordItem(){
-  
-  const diceValue = getNode('#cube').dataset.dice / 1;
+function renderRecordItem() {
+  const diceValue = memo("cube").dataset.dice / 1;
 
-  insertLast('.recordList tbody',createItem(diceValue));
-
+  insertLast(".recordList tbody", createItem(diceValue));
+  endScroll(recordListWrapper);
 }
 
-const handleRollingDice = (()=>{
-
+const handleRollingDice = (() => {
   let isClicked = false;
   let stopAnimation;
 
-  return ()=>{
-    if(!isClicked){ // 실행
+  return () => {
+    if (!isClicked) {
+      // 실행
       stopAnimation = setInterval(diceAnimation, 100);
       recordButton.disabled = true;
       resetButton.disabled = true;
-    }else{ // 정지
+    } else {
+      // 정지
       clearInterval(stopAnimation);
       recordButton.disabled = false;
       resetButton.disabled = false;
     }
-  
-    isClicked = !isClicked
-  }
-})()
 
-function handleRecord(){
+    isClicked = !isClicked;
+  };
+})();
+
+function handleRecord() {
   recordListWrapper.hidden = false;
 
-  renderRecordItem()
-
+  renderRecordItem();
 }
 
-function handleReset(){
+function handleReset() {
   recordListWrapper.hidden = true;
+
+  clearContents(getNode("tbody"));
+
+  count = 0;
+  total = 0;
 }
 
-rollingButton.addEventListener('click',handleRollingDice)
-recordButton.addEventListener('click',handleRecord)
-resetButton.addEventListener('click',handleReset)
-
-
-
-
-
-
-
-
+rollingButton.addEventListener("click", handleRollingDice);
+recordButton.addEventListener("click", handleRecord);
+resetButton.addEventListener("click", handleReset);
