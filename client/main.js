63244191c77671031d53cff78ceba09e9b7d6bee
kindtiger <seonbeom2@gmail.com>
2023-12-21 15:51:49 +0900

    [learn] localStorage 끝

--- client/main.js
+++ client/main.js
@@ -4,8 +4,12 @@
 
 
 
-import {  } from "./lib/index.js";
+import { getNode, getStorage, setStorage } from "./lib/index.js";
 
+
+
+
+const textField = getNode('#textField');
 
 
 
@@ -13,12 +17,27 @@ import {  } from "./lib/index.js";
 
 
 
+// 2. 새로고침시(DOMContentLoaded) 로컬스토리지에 저장된 textField의 값을 가져와 뿌려줍니다.
 
 
 
+function handleTextField(){
+  
+  const value = this.value;
+  setStorage('text',value);
+
+}
+
+function init(){
+
+  getStorage('text').then(res => textField.value = res)
+
+}
 
 
 
+textField.addEventListener('input',handleTextField)
+window.addEventListener('DOMContentLoaded',init)
 
 
 
